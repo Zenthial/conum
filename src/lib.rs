e446bccc66385e65ebdcdb7ec8d01f988a6c1211
Thomas Schollenberger <tschollenberger@sei.cmu.edu>
2023-07-25 13:31:22 -0400

    basic number functionality, add/sub

---
+++ src/lib.rs
@@ -0,0 +1,216 @@
+#![deny(clippy::pedantic, clippy::perf, clippy::style)]
+#![allow(unused)]
+use std::ops::{Add, Sub};
+
+// const B: u64 = u32::MAX as u64;
+const B: u64 = 10;
+
+fn digits_inner(n: u32, xs: &mut Vec<u32>) {
+    if n >= 10 {
+        digits_inner(n / 10, xs);
+    }
+    xs.push(n % 10);
+}
+
+fn to_digits(n: u32) -> Vec<u32> {
+    let mut xs = Vec::new();
+    digits_inner(n, &mut xs);
+    xs
+}
+
+pub struct Num<const WIDTH: usize> {
+    pub(crate) digits: Box<[u32; WIDTH]>,
+    pub(crate) msd: usize,
+    pub(crate) neg: bool,
+}
+
+impl<const WIDTH: usize> Num<WIDTH> {
+    #[must_use]
+    pub fn new(digits: &[u32]) -> Self {
+        let mut dig = [0_u32; WIDTH];
+
+        // digits.reverse();
+        for (i, d) in digits.iter().enumerate() {
+            dig[WIDTH - (digits.len() - i)] = *d;
+        }
+
+        Self {
+            digits: Box::new(dig),
+            msd: WIDTH - digits.len(),
+            neg: false,
+        }
+    }
+
+    fn update_msd(&mut self) {
+        for i in 0..self.digits.len() {
+            let d = self.digits[i];
+            if d != 0 {
+                self.msd = i;
+                break;
+            }
+        }
+    }
+}
+
+impl<const WIDTH: usize> Add for Num<WIDTH> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut r: Num<WIDTH> = Self::new(&[]);
+        let mut carry = false;
+
+        for i in (self.msd..WIDTH).rev() {
+            let mut d = u64::from(self.digits[i]);
+
+            if carry || i >= rhs.msd {
+                if carry {
+                    d += 1;
+                }
+
+                if i >= rhs.msd {
+                    d += u64::from(rhs.digits[i]);
+                }
+
+                if d >= B {
+                    carry = true;
+                    d -= B;
+                } else {
+                    carry = false;
+                }
+            }
+
+            r.digits[i] = u32::try_from(d).expect("overflow error");
+        }
+
+        if carry {
+            if self.msd == 0 {
+                panic!("add overflow");
+            } else {
+                r.digits[self.msd - 1] = 1;
+                r.msd = self.msd - 1;
+            }
+        } else {
+            r.msd = self.msd;
+        }
+
+        r
+    }
+}
+
+impl<const WIDTH: usize> Sub for Num<WIDTH> {
+    type Output = Self;
+
+    fn sub(self, rhs: Num<WIDTH>) -> Self::Output {
+        let mut r: Num<WIDTH> = Self::new(&[]);
+        let mut borrow = false;
+
+        for i in (self.msd..WIDTH).rev() {
+            let mut d = i64::from(self.digits[i]);
+
+            if borrow || i >= rhs.msd {
+                if borrow {
+                    d -= 1;
+                }
+
+                if i >= rhs.msd {
+                    d -= i64::from(rhs.digits[i]);
+                }
+
+                if d < 0 {
+                    borrow = true;
+                    d += i64::try_from(B).unwrap(); // will never fail
+                } else {
+                    borrow = false;
+                }
+            }
+
+            r.digits[i] = u32::try_from(d).expect("subtraction failed");
+        }
+
+        r.update_msd();
+        r
+    }
+}
+
+impl<const WIDTH: usize> From<u32> for Num<WIDTH> {
+    fn from(n: u32) -> Self {
+        let digits = to_digits(n);
+        Self::new(&digits)
+    }
+}
+
+impl<const WIDTH: usize> From<String> for Num<WIDTH> {
+    fn from(s: String) -> Self {
+        let digits: Vec<u32> = s
+            .chars()
+            .collect::<Vec<char>>()
+            .iter()
+            .map(|c| c.to_digit(10).expect("not a digit"))
+            .collect();
+        Self::new(&digits)
+    }
+}
+
+impl<const WIDTH: usize> From<&str> for Num<WIDTH> {
+    fn from(s: &str) -> Self {
+        Self::from(s.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_msd_width_3() {
+        let num: Num<3> = Num::new(&[1, 2, 3]);
+        assert_eq!(0, num.msd);
+    }
+
+    #[test]
+    fn check_msd_width_200() {
+        let num: Num<200> = Num::new(&[1, 2, 3]);
+        assert_eq!(197, num.msd);
+        assert_eq!(1, num.digits[num.msd]);
+        assert_eq!(2, num.digits[num.msd + 1]);
+        assert_eq!(3, num.digits[num.msd + 2]);
+    }
+
+    #[test]
+    fn check_from() {
+        let num1: Num<200> = Num::new(&[1, 2, 3]);
+        let num2: Num<200> = Num::from(123);
+        assert_eq!(num1.digits, num2.digits);
+    }
+
+    #[test]
+    fn check_from_string() {
+        let num1: Num<200> = Num::from(123);
+        let num2: Num<200> = Num::from("123");
+        assert_eq!(num1.digits, num2.digits);
+    }
+
+    #[test]
+    fn test_add() {
+        let num1: Num<200> = Num::from(123);
+        let num2: Num<200> = Num::from(123);
+        let expected_result: Num<200> = Num::from(123 + 123);
+        assert_eq!((num1 + num2).digits, expected_result.digits);
+    }
+
+    #[test]
+    fn test_sub() {
+        let num1: Num<200> = Num::from(123);
+        let num2: Num<200> = Num::from(100);
+        let expected_result: Num<200> = Num::from(123 - 100);
+        assert_eq!((num1 - num2).digits, expected_result.digits);
+    }
+
+    #[test]
+    fn test_add_overflow() {
+        let num1: Num<200> = Num::from(129);
+        let num2: Num<200> = Num::from(123);
+        let expected_result: Num<200> = Num::from(129 + 123);
+        assert_eq!((num1 + num2).digits, expected_result.digits);
+    }
+}
